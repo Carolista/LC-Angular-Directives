238a86080779853c04fbae615f47232ea5c8d090
Carolista <cjones.renaissancewoman@gmail.com>
2020-11-06 00:58:03 -0600

    added Food class; used ngFor to iterate through food items; added content to aside

--- angular-forms-directives/src/app/savory/savory.component.ts
+++ angular-forms-directives/src/app/savory/savory.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Food } from '../food';
 
 @Component({
-  selector: 'app-savory',
-  templateUrl: './savory.component.html',
-  styleUrls: ['./savory.component.css']
+    selector: 'app-savory',
+    templateUrl: './savory.component.html',
+    styleUrls: ['./savory.component.css']
 })
 export class SavoryComponent implements OnInit {
 
-  constructor() { }
+    allSavories: Food[] = [
+        new Food("Stuffed Squash", "../assets/images/stuffed-squash.jpg", 3.99),
+        new Food("Shrimp Salad", "../assets/images/shrimp-salad.jpg", 5.49),
+        new Food("Mac & Cheese", "../assets/images/mac-cheese.jpg", 2.99)
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
--- angular-forms-directives/src/app/sweet/sweet.component.ts
+++ angular-forms-directives/src/app/sweet/sweet.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Food } from '../food';
 
 @Component({
-  selector: 'app-sweet',
-  templateUrl: './sweet.component.html',
-  styleUrls: ['./sweet.component.css']
+    selector: 'app-sweet',
+    templateUrl: './sweet.component.html',
+    styleUrls: ['./sweet.component.css']
 })
 export class SweetComponent implements OnInit {
 
-  constructor() { }
+    allSweets: Food[] = [
+        new Food("Cinnamon Rolls", "../assets/images/cinnamon-rolls.jpg", 1.99, 20),
+        new Food("Monster Cookies", "../assets/images/monster-cookies.jpg", 1.59, 15),
+        new Food("Divinity", "../assets/images/divinity.jpg", 0.99, 10)
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
